7de0649eba3d6516ca1d9d65f8228ef13de14c54
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-03-02 13:11:52 +0900

    routes編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    passwords: 'users/passwords',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end

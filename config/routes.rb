439680283fe756eb0d39506fcbfa02dfd9f5cbd9
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-03-06 19:36:43 +0900

    routs.rb変更（user追加）

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: "tops#index"
 
-  # devise_for :users, controllers: {
-  #   passwords: 'users/passwords',
-  #   sessions: 'users/sessions',
-  #   registrations: 'users/registrations'
-  # }
-  # devise_scope :user do
-  #   post 'users/guest_sign_in', to: 'users/sessions#new_guest'
-  # end
+  devise_for :users, controllers: {
+    passwords: 'users/passwords',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end

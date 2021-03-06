70662770491a748897b3ede892693d154372c862
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-03-06 19:27:22 +0900

    nodeバージョン変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-// import '../stylesheets/application.scss'
+import '../stylesheets/application.scss'
 import "jquery"
 import "../modules/jquery.bgswitcher"
 
@@ -15,5 +15,4 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-require ('../stylesheets/application.scss')
 require ('tops')

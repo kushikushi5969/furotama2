8c69fd296ad31119f08e2c1bef3744d7ba1feb76
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-03-07 23:04:55 +0900

    favorite_requestテスト作成実行

--- spec/requests/favorites_request_spec.rb
+++ spec/requests/favorites_request_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "Favorites", type: :request do
+  before do
+    @user = FactoryBot.create(:user)
+    @area = FactoryBot.create(:area)
+    @test_post = FactoryBot.create(:post, user: @user, area: @area)
+    sign_in @user
+  end
+  let(:favo_params) { { user_id: @user.id, post_id: @test_post.id } }
+
+  describe "create" do
+    example "お気に入りすること" do
+      expect do
+        post post_favorites_path(@test_post.id), params: { favorite: favo_params, format: :js }
+      end.to change(Favorite, :count).by(1)
+    end
+  end
+
+  describe "destroy" do
+    let!(:favorite) { create(:favorite, user_id: @user.id, post_id: @test_post.id) }
+    example "お気に入り解除できること" do
+      expect do
+        delete post_favorite_path(post_id: @test_post.id, id: favorite.id), params: { favorite: favorite.id, format: :js }
+      end.to change(Favorite, :count).by(-1)
+    end
+  end
 
 end

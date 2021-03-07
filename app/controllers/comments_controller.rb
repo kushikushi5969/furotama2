c74da53b5e41340359ef74cef8502eea9ff6a85f
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-03-07 19:51:40 +0900

    ajaxコメント投稿・削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,23 +1,28 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[destroy]
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
     @comment.user_id = current_user.id
     @comment.save
     @post.create_notification_comment!(current_user, @comment.id)
-    render :index
+    render :create
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@post.id), alert: 'コメントを削除しました'
+    # render :destroy
+    # redirect_to post_path(@post.id), alert: 'コメントを削除しました'
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:text, :user_id, :post_id)
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
   end
 end
